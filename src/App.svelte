3b0641ab6522ba50876f8d4499ce93495dc940da
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2021-03-27 20:10:45 +0100

    add app bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,65 @@
 <script>
   import Card from "./Card.svelte";
+  import {MaterialApp, AppBar, Button, Icon, Menu, ListItem} from 'svelte-materialify';
+
   let selectedCard;
 
   const onCardSelection = (x) => {
     console.log(x.detail.name);
     selectedCard = x.detail.name;
   }
+
+  let theme = 'dark';
 </script>
 
+
 <main>
-  <h1>Oh my talks!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build
-    Svelte apps.</p>
+  <MaterialApp theme="{theme}">
+    <AppBar>
+      <div slot="icon">
+        <Button fab depressed>
+          <!--          <Icon path={mdiMenu} />-->
+        </Button>
+      </div>
+      <span slot="title">Title</span>
+      <div style="flex-grow:1"/>
+      <Button>Item</Button>
+      <Menu right>
+        <div slot="activator">
+          <Button fab depressed>
+            <!--            <Icon path={mdiDotsVertical} />-->
+          </Button>
+        </div>
+        <ListItem>Item 1</ListItem>
+        <ListItem>Item 2</ListItem>
+        <ListItem>Item 3</ListItem>
+      </Menu>
+    </AppBar>
+    <h1>Oh my talks!</h1>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build
+      Svelte apps.</p>
 
-  <Card name="JS1" on:click={onCardSelection}></Card>
-  <Card name="JS2" on:click={onCardSelection}></Card>
-  <Card name="JS3" on:click={onCardSelection}></Card>
+    <Card name="JS1" on:click={onCardSelection}></Card>
+    <Card name="JS2" on:click={onCardSelection}></Card>
+    <Card name="JS3" on:click={onCardSelection}></Card>
 
-  {#if selectedCard}
-    <div>
-      {selectedCard}
-    </div>
-  {/if}
+    {#if selectedCard}
+      <div>
+        {selectedCard}
+      </div>
+    {/if}
+  </MaterialApp>
 </main>
 
 <style>
   main {
+    padding: 0;
     text-align: center;
-    padding: 1em;
+    /*padding: 1em;*/
     max-width: 240px;
-    margin: 0 auto;
+    /*margin: 0 auto*/
+    height: 100%;
+
   }
 
   h1 {
