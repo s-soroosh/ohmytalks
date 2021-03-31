f8fc8df052823f21ef65c5bdc65e62e8fe8af362
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2021-03-31 14:11:56 +0200

    svelte description

--- src/data/courses.js
+++ src/data/courses.js
@@ -35,7 +35,7 @@ export default [
   {
     "slug": "javascript5",
     "title": "Javascript Bootcamp - Part 5",
-    "summary":"",
+    "summary": "",
     "description": `
       <p>
           در این قسمت با کلمات کلیدی async/await در جاوااسکریپت آشنا میشید.
@@ -51,8 +51,17 @@ export default [
   {
     "slug": "svelte1",
     "title": "Svelte Bootcamp - Part 1\n",
-    "summary":"",
-    "description": "",
+    "summary": "",
+    "description": `
+    <p>
+      Sevelte یک وب فریمورک بسیار ساده و جذاب است که به شما امکان ساخت وب سایت و یا وب اپلیکشن مورد نظرتون رو با کمترین میزان لازم کد می‌دهد.
+      این فریمورک نسبت به رقبای خود سرعت بهتری دارد و یادگیری و کار با آن بسیار ساده و شیرین است.
+      
+      </p>
+      <p>  
+      در این قسمت از آموزش فریمورک Svelete شما یاد خواهید گرفت که چطور محیط برنامه‌نویسی را اماده کنید و همینطور با بخشی از امکانات ابتدایی این فریمورک آشنا خواهید شد.
+    </p>
+    `,
     "link": "https://www.youtube.com/embed/57scZ70ehuM",
     "keywords": ["web framework", "sevelte", "SPA"],
     "image": "/images/svelte.png"
