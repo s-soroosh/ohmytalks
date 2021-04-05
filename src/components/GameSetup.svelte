6298bf7e2c2140cde8511e607340453e3ad8e4d0
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2021-04-05 18:10:16 +0200

    login/register for xo game

---
+++ src/components/GameSetup.svelte
@@ -0,0 +1,33 @@
+<script>
+  import {game, user} from "./store"
+
+  function newGame() {
+    game.newGame($user.jwt)
+  }
+</script>
+
+<div class="form">
+  {#if !$game}
+    <div>
+      <h3>Click here to create a new game</h3>
+      <input type="button" value="New Game" on:click={newGame}/>
+    </div>
+    <div>
+      <h3>Please fill the code if you already have one</h3>
+      <input type="text" value=""/>
+      <input type="btton" value="Join Game">
+    </div>
+  {/if}
+  {#if $game}
+    Your game code is {$game.code}, feel free to share with anyone you want to play with.
+  {/if}
+
+</div>
+
+<style>
+  .form {
+    background-color: #ffffff;
+    padding: 20px;
+
+  }
+</style>
---
+++ src/routes/xo/index.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Login from "../../components/Login.svelte";
+  import {user} from "../../components/store"
+  import GameSetup from "../../components/GameSetup.svelte";
+
+</script>
+
+<div class="content">
+  {#if !$user}
+    <Login/>
+  {/if}
+  {#if $user}
+    <GameSetup />
+  {/if}
+
+</div>
+
+
+<style>
+  .content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #999999;
+  }
+</style>
